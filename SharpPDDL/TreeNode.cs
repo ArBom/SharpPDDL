f5e50ff9e308f15ab201f759b32f500b8894770e
Arek Bomba <59375967+ArBom@users.noreply.github.com>
2024-02-29 21:18:47 +0100

    Implementation of  ICollection<T> interface at TreeNode<T> class

--- SharpPDDL/TreeNode.cs
+++ SharpPDDL/TreeNode.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace SharpPDDL
 {
     internal class TreeNode<T> : ICollection<T> where T : class
-    { 
+    {
         public T Content;
         public TreeNode<T> Root;
         public List<TreeNode<T>> Children;
@@ -18,46 +18,7 @@ namespace SharpPDDL
             this.Content = null;
         }
 
-        public bool IsReadOnly => false;
-
-        public TreeNode<T> this[int key]
-        {
-            get { return this.Children[key]; }
-            set { this.Children[key] = value; }
-        }
-
-        private List<int> Position(List<int> previesly)
-        {
-            if (this.Root == null)
-                return previesly;
-
-            List<int> current = new List<int>(this.Root.Children.IndexOf(this));
-            current.AddRange(previesly);
-            return this.Root.Position(current);
-        }
-
-        public List<int> Position()
-        {
-            if (this.Root == null)
-                return null;
-
-            List<int> ToRet = new List<int>(this.Root.Children.IndexOf(this));
-
-            return Position(ToRet);
-        }
-
-        public int Count
-        {
-            get
-            {
-                int count = 1;
-                foreach (var child in this.Children)
-                {
-                    count = count + child.Count;
-                }
-                return count;
-            }
-        }
+        #region ICollection
 
         public void Add(T item)
         {
@@ -68,15 +29,6 @@ namespace SharpPDDL
             };
 
             this.Children.Add(AddedItem);
-        }
-
-        internal void AddAbove(TreeNode<T> newParent)
-        {
-            int index = this.Root.Children.IndexOf(this);
-            newParent.Root = this.Root;
-            newParent.Children.Add(this);
-            this.Root.Children[index] = newParent;
-            this.Root = newParent;
         }
 
         public void Clear()
@@ -105,6 +57,87 @@ namespace SharpPDDL
             return false;
         }
 
+        public int Count
+        {
+            get
+            {
+                int count = 1;
+                foreach (var child in this.Children)
+                {
+                    count = count + child.Count;
+                }
+                return count;
+            }
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            ToArray().CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            TreeNodeEnum<T> ToRet = new TreeNodeEnum<T>(this);
+            return ToRet;
+        }
+
+        public bool IsReadOnly => false;
+
+        public bool Remove(T item)
+        {
+            if (this.Content.Equals(item))
+                return this.Remove();
+
+            foreach (var child in this.Children)
+                if (child.Remove(item))
+                    return true;
+
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+        #endregion
+
+        public TreeNode<T> this[int key]
+        {
+            get { return this.Children[key]; }
+            set { this.Children[key] = value; }
+        }
+
+        private List<int> Position(List<int> previesly)
+        {
+            if (this.Root == null)
+                return previesly;
+
+            List<int> current = new List<int>(this.Root.Children.IndexOf(this));
+            current.AddRange(previesly);
+            return this.Root.Position(current);
+        }
+
+        public List<int> Position()
+        {
+            if (this.Root == null)
+                return null;
+
+            List<int> ToRet = new List<int>(this.Root.Children.IndexOf(this));
+
+            return Position(ToRet);
+        }
+
+        internal void AddAbove(TreeNode<T> newParent)
+        {
+            int index = this.Root.Children.IndexOf(this);
+            newParent.Root = this.Root;
+            newParent.Children.Add(this);
+            this.Root.Children[index] = newParent;
+            this.Root = newParent;
+        }
+
+
         private void MoveNodesToList(TreeNode<T> node, List<T> resultList)
         {
             resultList.Add(node.Content);
@@ -127,22 +160,12 @@ namespace SharpPDDL
             return resultList.ToArray();
         }
 
-        public void CopyTo(T[] array, int arrayIndex)
-        {
-            ToArray().CopyTo(array, arrayIndex);
-        }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
         public bool Remove()
         {
             if (this.Root is null)
                 return false;
 
-            for(int childCount = this.Children.Count; childCount >= 0; childCount--)
+            for (int childCount = this.Children.Count; childCount >= 0; childCount--)
             {
                 this.Root.Children.Add(this.Children[childCount]);
                 this.Children[childCount].Root = this.Root;
@@ -153,22 +176,59 @@ namespace SharpPDDL
 
             return true;
         }
+    }
 
-        public bool Remove(T item)
+    internal class TreeNodeEnum<T> : IEnumerator<T> where T : class
+    {
+        private TreeNode<T> current;
+
+        internal TreeNodeEnum(TreeNode<T> creator)
         {
-            if (this.Content.Equals(item))
-                return this.Remove();
+            this.current = creator;
+        }
 
-            foreach (var child in this.Children)
-                if (child.Remove(item))
+        public object Current => current;
+
+        T IEnumerator<T>.Current => current.Content;
+
+        private bool MoveNextFromLine(TreeNode<T> e)
+        {
+            if (!(e.Root is null))
+            {
+                int IndeksOfE = e.Root.Children.IndexOf(e);
+
+                if (e.Root.Children.Count != ++IndeksOfE)
+                {
+                    e = e.Root.Children[IndeksOfE];
                     return true;
-
+                }
+                else
+                {
+                    return MoveNextFromLine(e.Root);
+                }
+            }
             return false;
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
+        public bool MoveNext()
         {
-            throw new NotImplementedException();
+            if (current.Children.Count != 0)
+            {
+                current = current.Children[0];
+                return true;
+            }
+
+            return MoveNextFromLine(current);
         }
+
+        public void Reset()
+        {
+            while (current.Root != null)
+            {
+                current = current.Root;
+            }
+        }
+
+        void IDisposable.Dispose() { }
     }
 }
